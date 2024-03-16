ea2615acee6889a1800fbf488e000126698f3d63
k3rn3lpanic <matin.ghiasvand1381@gmail.com>
2024-03-16 22:00:10 +0330

    Update Shop.ts

--- test/Shop.ts
+++ test/Shop.ts
@@ -10,17 +10,6 @@ describe("Shop", function(){
         const shopAddress = await deployer.shopAddresses(Number(await deployer.shopCount()) - 1);
         const nftAddress = await deployer.nftContracts(Number(await deployer.shopCount()) - 1);
         console.log(shopAddress, nftAddress);
-        // const Shop = await ethers.getContractFactory("Shop");
-        // const shop = await Token.deploy(1e9);
-        // const NFT = await ethers.getContractFactory("ExModulesNFT");
-        // const nft = await NFT.deploy("Exmodule NFT", "exm");
-        // const Market = await ethers.getContractFactory("ExmodulesMarketPlace");
-        // const market = await Market.deploy(await token.getAddress(), 100, owner);
-        // await nft.waitForDeployment();
-        // await token.waitForDeployment();
-        // await market.waitForDeployment();
-        // token.connect(owner).setMarketPlaceContract(await market.getAddress());
-        // return {token, owner, firstUser, secondUser, nft, market};
     }
 
     it("Should deploy shop", async function () {
