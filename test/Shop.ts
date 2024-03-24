5786647440d4d290a915e76e4462385b110105d1
k3rn3lpanic <matin.ghiasvand1381@gmail.com>
2024-03-25 02:19:27 +0330

    Added 3 more tests to Shop.ts

--- test/Shop.ts
+++ test/Shop.ts
@@ -16,18 +16,54 @@ describe("Shop", function(){
     beforeEach(async function() {
         [owner,firstUser,secondUser] = await ethers.getSigners();
         const Deployer = await ethers.getContractFactory("DropShopDeployer");
-        deployer = await Deployer.deploy();
-        await deployer.connect(owner).deployShop("ShopName", "Address", await owner.getAddress(), "LogoUrl", "Description");
+        deployer = await Deployer.deploy(120);
+        await deployer.connect(owner).deployShop("ShopName", "Address", "LogoUrl", "Description");
         shopAddress = await deployer.shopAddresses(Number(await deployer.shopCount()) - 1);
         nftAddress = await deployer.nftContracts(Number(await deployer.shopCount()) - 1);
         nftContract = await ethers.getContractAt("DroplinkedToken", nftAddress);
         shopContract = await ethers.getContractAt("DropShop", shopAddress);
     });
 
-    it("Should deploy shop", async function () {
-        console.log(`NFT deployed to: ${await nftContract.getAddress()}`);
-        console.log(`Shop deployed to: ${await shopContract.getAddress()}`);
+    describe("Deployment", function(){
+        it("Should deploy shop", async function () {
+            console.log(`NFT deployed to: ${await nftContract.getAddress()}`);
+            console.log(`Shop deployed to: ${await shopContract.getAddress()}`);
+        });
+        
+        it("Should set the right fee", async function() {
+            expect(await deployer.droplinkedFee()).to.equal(100);
+        });
     })
-
     
+
+    describe("Set & Update heartbeat", function(){
+        it("Should update the heartbeat with owner account", async function(){
+            await deployer.connect(owner).setHeartBeat(4000);
+            expect(await deployer.getHeartBeat()).to.equal(4000);
+        });
+        it("should not update the heartbeat with other account", async function(){
+            await expect(deployer.connect(firstUser).setHeartBeat(4000)).to.be.revertedWithCustomError(deployer, "OwnableUnauthorizedAccount");
+        });
+    });
+
+    describe("Set & update fee", function(){
+        it("Should update the fee to given number using owner account", async function(){
+            await deployer.connect(owner).setDroplinkedFee(200);
+            expect(await deployer.getDroplinkedFee()).to.equal(200);
+        });
+        it("Should not update the fee to given number using other account", async function(){
+            await expect(deployer.connect(firstUser).setDroplinkedFee(200)).to.be.revertedWithCustomError(deployer, "OwnableUnauthorizedAccount");
+        });
+    });
+
+    describe("Mint", function(){
+        it("Should mint 5000 tokens", async function(){
+            enum ProductType {
+                DIGITAL,
+                POD,
+                PHYSICAL
+            };
+            await nftContract.connect(owner).mint();
+        });
+    });
 });
